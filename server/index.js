94ff6b2840bf99093de9d36b1b0e58dcc913677d
Quyen Hoang <qduhoang@gmail.com>
2023-08-15 10:40:58 -0700

    JSON data complete in local development

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const data = require('../public/data.json');
+
+const app = express();
+
+// https://stackoverflow.com/questions/32679505/node-and-express-send-json-formatted
+app.set('json spaces', 2);
+
+app.get('/', (req, res) => {
+  res.status(200).json(data);
+});
+
+let PORT = process.env.PORT || 8080;
+app.listen(8080, () => {
+  console.log(`Server now listening on PORT ${PORT}`);
+});
